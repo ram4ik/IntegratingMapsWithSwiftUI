ac0e9040083694a078c1af6640dd4a131db7d732
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-01-30 10:15:46 +0200

    Zooming in Maps Using SwiftUI

--- IntegratingMapsWithSwiftUI/Coordinators/Coordinator.swift
+++ IntegratingMapsWithSwiftUI/Coordinators/Coordinator.swift
@@ -17,4 +17,16 @@ final class Coordinator: NSObject, MKMapViewDelegate {
         self.control = control
     }
     
+    func mapView(_ mapView: MKMapView, didAdd views: [MKAnnotationView]) {
+        if let annotationView = views.first {
+            if let annotation = annotationView.annotation {
+                if annotation is MKUserLocation {
+                    
+                    let region = MKCoordinateRegion(center: annotation.coordinate, latitudinalMeters: 1000, longitudinalMeters: 1000)
+                    
+                    mapView.setRegion(region, animated: true)
+                }
+            }
+        }
+    }
 }
